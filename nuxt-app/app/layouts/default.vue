728e45c11b3078177f04fb4b81654a9cc94fb76d
yuutsu <nurikbaidibek@gmail.com>
2026-05-17 23:50:49 +0500

    feat(nuxt): add i18n sup

--- nuxt-app/app/layouts/default.vue
+++ nuxt-app/app/layouts/default.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import ru from "element-plus/es/locale/lang/ru";
+import en from "element-plus/es/locale/lang/en";
+import kk from "element-plus/es/locale/lang/kk";
+
+type langsT = "ru" | "en" | "kk";
+const langs = { ru, en, kk };
+const locale = ref(ru);
+
+const switchLang = (lang: langsT) => {
+	locale.value = langs[lang];
+};
+</script>
+
 <template>
 	<div>
 		HEADER
