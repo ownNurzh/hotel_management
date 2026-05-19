02082a429fc4dac7d79e65ad71269e0de9ff2ca5
yuutsu <nurikbaidibek@gmail.com>
2026-05-19 21:42:52 +0500

    refactor(electron): change main file path

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,7 @@
-const { contextBridge } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("versions", {
-	node: () => process.versions.node,
-	chrome: () => process.versions.chrome,
-	electron: () => process.versions.electron,
+contextBridge.exposeInMainWorld("auth", {
+	login: (login, password) => {
+		ipcRenderer.invoke("auth:login", login, password);
+	},
 });
