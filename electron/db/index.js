1d562cea0cd528a0e43ed7cb86e0f9f57e9ec779
yuutsu <nurikbaidibek@gmail.com>
2026-05-20 13:18:06 +0500

    chore: delete drizzle-orm depend

--- electron/db/index.js
+++ electron/db/index.js
@@ -2,27 +2,11 @@ const { DatabaseSync } = require("node:sqlite");
 
 const fs = require("node:fs");
 const path = require("node:path");
-const { drizzle } = require("drizzle-orm/sqlite-proxy");
 const config = require("../config");
-const schema = require("./schema");
 
-const sqlite = new DatabaseSync(path.join(config.userDataPath, "hotel.db"));
+const db = new DatabaseSync(path.join(config.userDataPath, "hotel.db"));
 
 const initSQL = fs.readFileSync(path.join(__dirname, "init.sql"), "utf-8");
-sqlite.exec(initSQL);
-
-const db = drizzle(
-	(sql, params, method) => {
-		const stmt = sqlite.prepare(sql);
-		if (method === "run") {
-			stmt.run(...params);
-			return { rows: [] };
-		}
-		const rows =
-			method === "get" ? [stmt.get(...params)] : stmt.all(...params);
-		return { rows: rows.filter(Boolean) };
-	},
-	{ schema },
-);
+db.exec(initSQL);
 
 module.exports = { db };
