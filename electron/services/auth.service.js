08b72729dba74915c6f69e5023bb28e61208e316
yuutsu <nurikbaidibek@gmail.com>
2026-05-21 15:27:44 +0500

    feat(electron): add session service

--- electron/services/auth.service.js
+++ electron/services/auth.service.js
@@ -1,10 +1,16 @@
 class Auth {
-	constructor(userService) {
+	constructor(userService, sessionService) {
 		this.userService = userService;
+		this.sessionService = sessionService;
 	}
 	login(login, password) {
 		let hash_password = password;
-		return this.userService.login(login, hash_password);
+		const result = this.userService.login(login, hash_password);
+		const answer = result != undefined;
+		if (answer) {
+			this.sessionService.save({ login: result.login });
+		}
+		return answer;
 	}
 	register(first_name, second_name, login, password, role_id) {
 		let hash_password = password;
---
+++ electron/services/session.service.js
@@ -0,0 +1,23 @@
+const Store = require("electron-store");
+class Session {
+	constructor() {
+		this.store = new Store();
+		this.sessiongName = "session";
+	}
+	save(data) {
+		this.store.set(this.sessiongName, data);
+	}
+
+	get() {
+		return this.store.get(this.sessiongName, null);
+	}
+
+	clear() {
+		this.store.delete(this.sessiongName);
+	}
+
+	isLoggedIn() {
+		return this.store.has(this.sessiongName);
+	}
+}
+module.exports = Session;
