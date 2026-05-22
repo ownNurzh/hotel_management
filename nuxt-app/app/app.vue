0fe984961a1949b2c4c3d9068d2ae9ec0b9a08b5
yuutsu <nurikbaidibek@gmail.com>
2026-05-22 19:21:26 +0500

    feat(nuxt): add app spinner loader

--- nuxt-app/app/app.vue
+++ nuxt-app/app/app.vue
@@ -1,5 +1,41 @@
+<script setup>
+import { ElLoading } from "element-plus";
+
+const openFullScreen = () => {
+	const loading = ElLoading.service({
+		lock: true,
+		text: "Загружается...",
+		background: "rgba(33, 61, 91, 1)",
+		customClass: "hotel-loading",
+	});
+	setTimeout(() => {
+		loading.close();
+	}, 2500);
+};
+
+onMounted(() => {
+	openFullScreen();
+});
+</script>
 <template>
+	<NuxtLoadingIndicator />
 	<NuxtLayout>
 		<NuxtPage />
 	</NuxtLayout>
 </template>
+<style>
+.hotel-loading .el-loading-spinner .el-loading-text {
+	font-weight: bold;
+	color: white !important;
+	margin-top: 10px;
+}
+.hotel-loading .el-loading-spinner {
+	transform: scale(1.1);
+}
+
+.hotel-loading .el-loading-spinner .path {
+	stroke: white !important;
+	stroke-width: 3;
+	stroke-linecap: round;
+}
+</style>
--- nuxt-app/app/pages/index.vue
+++ nuxt-app/app/pages/index.vue
@@ -1,16 +1,1 @@
-<script setup>
-import { ElLoading } from "element-plus";
-
-const openFullScreen = () => {
-	const loading = ElLoading.service({
-		lock: true,
-		text: "Загружается...",
-		background: "rgba(0, 0, 0, 1)",
-	});
-	setTimeout(() => {
-		loading.close();
-	}, 2000);
-};
-openFullScreen();
-</script>
 <template></template>
