3ba8b362c4012d01808887dfb3bfe75ce64eeece
yuutsu <nurikbaidibek@gmail.com>
2026-05-24 19:32:20 +0500

    feat(electron): add room service

--- electron/services/index.js
+++ electron/services/index.js
@@ -2,11 +2,14 @@ const db = require("../db");
 const UserService = require("./user.service");
 const AuthService = require("./auth.service");
 const SessionService = require("./session.service");
+const RoomService = require("./room.service");
 
 const userService = new UserService(db);
 const sessionService = new SessionService();
+const roomService = new RoomService(db);
 module.exports = {
 	userService: userService,
 	authService: new AuthService(userService, sessionService),
 	sessionService: sessionService,
+	roomService: roomService,
 };
---
+++ electron/services/room.service.js
@@ -0,0 +1,26 @@
+class Room {
+	constructor(db) {
+		this.db = db;
+	}
+	createRoomType(name, price, capacity) {
+		return this.db
+			.prepare(
+				"INSERT INTO room_types (name,price,capacity) VALUES (?, ?, ?)",
+			)
+			.run(name, price, capacity);
+	}
+	getAllRoomTypes() {
+		return this.db.prepare("SELECT * FROM room_types").all();
+	}
+	//
+	createRoom(room_number, room_type_id) {
+		return this.db
+			.prepare(
+				"INSERT INTO rooms (room_number,room_type_id) VALUES (?, ?)",
+			)
+			.run(room_number, room_type_id);
+	}
+	getAllRooms() {
+		return this.db.prepare("SELECT * FROM rooms").all();
+	}
+}
